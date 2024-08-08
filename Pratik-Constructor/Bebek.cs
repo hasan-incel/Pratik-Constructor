2b55b290c64fca3f9958f4f297c6434f3477f495
Hasan İncel <h_incel@hotmail.com>
2024-08-08 09:35:57 +0300

    Add project files.

---
+++ Pratik-Constructor/Bebek.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_Constructor
+{
+    public class Bebek
+    {
+        public string Ad;
+        public string Soyad;
+        public DateTime DogumTarihi;
+
+        //Default constructor. Parametre almayan bir metot.
+        public Bebek()
+        {
+            Console.WriteLine("Ingaa!");
+            DogumTarihi = DateTime.Now;
+        }
+        // Ad ve Soyad parametreli constructor
+        public Bebek(string _ad, string _soyad)
+        {
+            Ad = _ad;
+            Soyad = _soyad;
+            Console.WriteLine("\nIngaa!");
+            DogumTarihi = DateTime.Now;
+        }
+
+        //Bebeğin bilgilerini yazdıram metot.
+        public void Bilgiler()
+        {
+            Console.WriteLine("Doğum tarihi: " + DogumTarihi);
+            Console.WriteLine("Adı: " + Ad);
+            Console.WriteLine("Soyadı: " + Soyad);
+        }
+    }
+}
---
+++ Pratik-Constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using Pratik_Constructor;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Parametresiz constructor ile bebek oluşturma
+        Bebek bebek1 = new Bebek();
+        bebek1.Ad = "Sabri";
+        bebek1.Soyad = "Sarıoğlu";
+        Console.WriteLine("-----Bebek 1-----");
+        bebek1.Bilgiler();
+
+        // Parametreli constructor ile bebek oluşturma
+        Bebek baby2 = new Bebek("Rıdvan", "Dilmen");
+        Console.WriteLine("-----Bebek 2-----");
+        baby2.Bilgiler();
+    }
+}
+
